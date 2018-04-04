c8f669992cf71675c96d4e84490b08cba8c1cba4
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-04 13:57:33 -0700

    adds first class StorePicker

---
+++ catch-of-the-day/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {render} from 'react-dom';
+
+//Storpicker component
+class StorePicker extends React.Component {
+    render(){
+        return <p>Hello</p>
+    }
+}
+
+render(<StorePicker/>, document.querySelector('#main'));
