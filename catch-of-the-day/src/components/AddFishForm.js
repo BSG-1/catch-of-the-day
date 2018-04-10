4393cbc9cc99f91f8c58c8adabcc2867d5a54330
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-10 12:03:01 -0700

    changes made to availability status

--- catch-of-the-day/src/components/AddFishForm.js
+++ catch-of-the-day/src/components/AddFishForm.js
@@ -11,12 +11,14 @@ class AddFishForm extends React.Component {
             desc: this.desc.value, 
             image: this.image.value, 
         }
+
         this.props.addFish(fish);
+        console.log(fish)
         this.fishForm.reset();
     }
 
     render(){
-        return(
+        return (
             <form ref={(input) => this.fishForm = input} className="fish-edit" onSubmit={(e) => this.createFish(e)}>
                 <input ref={(input) => this.name = input} type="text" placeholder="Fish Name"/>
                 <input ref={(input) => this.price = input} type="text" placeholder="Fish Price"/>
