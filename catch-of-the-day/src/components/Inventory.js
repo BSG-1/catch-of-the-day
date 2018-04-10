55449ae4d5b7227348d22655e2612437a6363552
Ghataorhe <cssb-ghataorhe@Ghataorhes-MacBook-Pro.local>
2018-04-10 14:06:29 -0700

    updates new value of fish

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -5,21 +5,38 @@ class Inventory extends React.Component {
     constructor() {
         super();
         this.renderInventory = this.renderInventory.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
+
+    handleChange(e, key) {
+        const fish = this.props.fishes[key];
+        console.log(fish);
+        //take a copy of that fish and update it with the new data
+        //console.log(e.target.name, e.target.value) <-- what this does is capture the actual element(in this case "name"), value (the new value you're putting there)
+        const updatedFish = {
+            ...fish,
+            //computed property
+            [e.target.name]: [e.target.value]
+        }
+        console.log(updatedFish)
+
+    }
+
     // inventory controller
     renderInventory(key) {
         //actually need the info about the fishes
         const fish = this.props.fishes[key];
 
         return (
-            <div name="fish-edit" key={key}>
-                <input type="text" name="name" value={fish.name} placeholder="name"/>
-                <input type="text" name="price" value={fish.price} placeholder="price"/>
-                <select type="text" name="status" value={fish.status} placeholder="status"/>
+            <div className="fish-edit" key={key}>
+                <input type="text" name="name" value={fish.name} placeholder="Fish name" onChange={(e) => this.handleChange(e, key)}/>
+                <input type="text" name="price" value={fish.price} placeholder="Fish price"/>
+                <select type="text" name="status" value={fish.status} placeholder="Fish status">
                     <option value="available">Fresh!</option>
                     <option value="unavailable">Sold Out!</option>
-                <textarea type="text" name="desc" value={fish.desc} placeholder="desc"></textarea>
-                <input type="text" name="image" value={fish.image} placeholder="image"/>
+                </select>
+                <textarea type="text" name="desc" value={fish.desc} placeholder="Fish desc"></textarea>
+                <input type="text" name="image" value={fish.image} placeholder="Fish Image"/>
             </div>
         )
     }
